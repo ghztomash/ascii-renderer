ec4a31303eae496a65adb696c4c5a05fd73c45b1
Tomash Ghz <ghz.tomash@hotmail.com>
2023-02-25 22:57:59 +0100

    Updated gitignore

--- bin/data/generate_loop.sh
+++ bin/data/generate_loop.sh
@@ -14,8 +14,6 @@ if [ $# -eq 2 ]
     FRAMERATE=$2
 fi
 
-
-
 echo -e "\nGenerating video file for project: $PROJECT\n"
 cd capture_$PROJECT
 ffmpeg -r $FRAMERATE -i "fbo_%d.jpg" -pix_fmt yuv420p -movflags +faststart video.mp4 -y 
