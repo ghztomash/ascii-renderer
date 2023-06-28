e0e19efe17592a484308fb001b06e3807fa24120
Tomash Ghz <ghz.tomash@hotmail.com>
2023-06-28 23:30:02 +0200

    common lua script variablesw

--- bin/data/scripts/common/variables.lua
+++ bin/data/scripts/common/variables.lua
@@ -2,7 +2,7 @@
 color = {r=0, g=0, b=0, a=0}
 dimensions = {x=100, y=100, z=0}
 canvasSize = {w=540, h=540}
-resolution = 1
-particles = 200
+resolution = 10
+particles = 100
 modulation = {}
 ----------------------------------------------------
